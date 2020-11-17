8e9c6ef9e7d72a05fd6424b386e39967dcf6803a
nicola.cristian <nicola.cristian@TAVE-PC19.OSDBSESTO.local>
2020-11-17 13:14:43 +0100

    bello

---
+++ JavaRepoTest/src/provagit/ProvaGit.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagit;
+
+/**
+ *
+ * @author nicola.cristian
+ */
+public class ProvaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+  int i =5;
+  Bello bello= new Bello();
+  
+  while(true){
+      System.out.println("Hello World");
+      System.out.println("Have a nice day "+bello.bello);
+      i++;  
+  }
+  
+ 
+
+    }
+    
+}
